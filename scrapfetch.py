5fa1e26112a577d70d04b5d635301a8454049f24
Jaspal Singh <jsingh49@ucmerced.edu>
2019-08-07 15:36:46 -0700

    Adding comments to the functions. Fixing up variable names so easier to understand.

--- scrapfetch.py
+++ scrapfetch.py
@@ -2,19 +2,38 @@
 import requests
 from bs4 import BeautifulSoup
 from file_read_backwards import FileReadBackwards
-import validators.url as url_valid
+from validators import url as url_valid
 
 def validate(url):
+    '''
+    Returns True if the url is valid, otherwise returns
+    :class: `~validators.utils.ValidationFailure'.
+    '''
     return url_valid(url)
 
 def content_fetch(url):
+    '''
+    Returns :class: `Response <Response>` object received for
+    the request that was made.
+    '''
     return requests.get(url)
 
-def request_successful(req):
-    return req.status_code == requests.codes.ok
+def request_successful(res):
+    '''
+    Takes in Response object as argument.
+    Return True if the request was successful, otherwise returns
+    False.
+    '''
+    return res.status_code == requests.codes.ok
 
-def fetch_links(file):
-    soup = BeautifulSoup(file.text, 'lxml')
+def fetch_links(res):
+    '''
+    Takes in Response object as argument.
+    Uses lxml XML toolkit to parse the response object with
+    BeautifulSoup.
+    Returns list of 'bs4.element.Tag'
+    '''
+    soup = BeautifulSoup(res.text, 'lxml')
     return [link for link in soup.find_all('a', class_='touch')]
 
 def give_choices(list_choices):
@@ -40,8 +59,9 @@ def write_to_file(list_choices):
     give_choices(list_choices)
 
     message = 'Please pick your choice: '
+    user_choices = list(map(str,range(1,len(list_choices)+1)))
 
-    user_choice = choose_from(message, *list(map(str,range(1,len(list_choices)+1))), 'end')
+    user_choice = choose_from(message, *user_choices, 'end')
 
     if user_choice == 'end':
         print("User chose not to proceed.")
@@ -55,7 +75,7 @@ def write_to_file(list_choices):
     filename = filename[-1]
 
     filename = input('Insert new name default({}): '.format(filename)) or filename
-    print("It's going to be saved as {}".format(filename))
+    print("It's going to be saved as '{}'".format(filename))
 
     if not filename.endswith('.extension'):
         filename = filename + '.extension'
@@ -67,15 +87,20 @@ def write_to_file(list_choices):
             return 'Successfully written to file.'
         else:
             print("Issues with file fetching")
-            print("Code Received: {}".format(req))
+            print("Code Received: '{}'".format(req))
     return
+
+def remove_from_file(remove_this):
+    pass
 
 def main():
     li_to_del = []
     message = "Work on the next line: "
+    user_choices = ['yes', 'no', 'skip']
+
     with FileReadBackwards('/path/to/file', encoding = 'utf-8') as file:
         for line in file:
-            user_input = choose_from(message, 'yes', 'no', 'skip')
+            user_input = choose_from(message, *user_choices)
             if user_input == 'yes':
                 link = line.strip()
                 if validate(link):
@@ -100,6 +125,8 @@ def main():
                     break
                 elif user_input == 'skip':
                     print("Skipping This\n")
+
+        remove_from_file(li_to_del)
 
 
 if __name__ == "__main__":
